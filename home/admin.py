25bbe827f8016863ee04e15da5d8bb2e0dd73e74
Devdeepsinh Zala <76090365+zaladevdeep@users.noreply.github.com>
2025-09-22 14:08:51 +0530

    Initial commit

--- home/admin.py
+++ home/admin.py
@@ -1,55 +1,24 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
 # Register your models here.
-from .models import (
-    ClientDetail,
-    CurrentIpoName,
-    CustomUser,
-    GroupDetail,
-    Order,
-    OrderDetail,
-    RateList,
-)
+from .models import CustomUser
+from .models import CurrentIpoName
+from .models import GroupDetail
+from .models import OrderDetail
+from .models import ClientDetail
+# from .models import PremiumOrderDetail
+# from .models import PremiumGroupDetail
+from .models import RateList
+from .models import Order
+from django.contrib.auth.admin import UserAdmin
 
 
 class UserModel(UserAdmin):
-    list_display = UserAdmin.list_display + (
-        "IPO_limit",
-        "Order_limit",
-        "Premium_Order_limit",
-        "Client_limit",
-        "Group_limit",
-        "Allotment_access",
-        "Expiry_Date",
-        "AppPassword",
-        "TelegramApi_id",
-        "TelegramApi_key",
-        "Mobileno",
-        "Telegram_session",
-    )
-
+    list_display = UserAdmin.list_display + ('IPO_limit','Order_limit','Premium_Order_limit','Client_limit','Group_limit','Allotment_access','Expiry_Date','AppPassword','TelegramApi_id','TelegramApi_key','Mobileno', 'Telegram_session')
+    
     fieldsets = UserAdmin.fieldsets + (
-        (
-            "Custom Fields",
-            {
-                "fields": (
-                    "IPO_limit",
-                    "Order_limit",
-                    "Premium_Order_limit",
-                    "Client_limit",
-                    "Group_limit",
-                    "Allotment_access",
-                    "Expiry_Date",
-                    "AppPassword",
-                    "TelegramApi_id",
-                    "TelegramApi_key",
-                    "Mobileno",
-                    "Telegram_session",
-                )
-            },
-        ),
+        ('Custom Fields', {'fields': ('IPO_limit','Order_limit','Premium_Order_limit','Client_limit','Group_limit','Allotment_access','Expiry_Date','AppPassword','TelegramApi_id','TelegramApi_key','Mobileno', 'Telegram_session')}),
     )
+    # field = ('Broker_id')
     pass
 
 
--- home/decorators.py
+++ home/decorators.py
@@ -1,6 +1,6 @@
-from django.contrib import messages
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.contrib import messages
 
 
 def allowed_users(allowed_roles=[]):
@@ -8,17 +8,17 @@ def allowed_users(allowed_roles=[]):
         def wrapper_func(request, *args, **kwargs):
 
             group = None
+            # if request.user.groups.exists():
             if request.user.is_anonymous:
-                messages.error(request, "Your session is expired. Please log in again.")
+                messages.error(
+                request, 'Your session is expired. Please log in again.')
                 return redirect("/login")
             group = request.user.groups.all()[0].name
             if group in allowed_roles:
                 return view_func(request, *args, **kwargs)
             else:
-                return HttpResponse("You are not authorized to view this page")
-
+                return HttpResponse('You are not authorized to view this page')
         return wrapper_func
-
     return decorator
 
 
@@ -28,11 +28,10 @@ def Broker_only(view_func):
         if request.user.groups.exists():
             group = request.user.groups.all()[0].name
 
-        if group == "Customer":
-            return redirect("/indexforCustomer")
+        if group == 'Customer':
+            return redirect('/indexforCustomer')
 
-        if group == "Broker":
+        if group == 'Broker':
             return view_func(request, *args, **kwargs)
-        return redirect("login")
-
+        return redirect('login')
     return wrapper_function
